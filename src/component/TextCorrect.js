d35960d4085104836db12e7ad5470a837c37c354
Evan M. Bonk <embonk@hotmail.com>
2025-08-07 14:15:44 -0600

    text correct

--- src/component/TextCorrect.js
+++ src/component/TextCorrect.js
@@ -8,7 +8,8 @@ function TextCorrect(text) {
     .replace(/&lt;/g, '<')
     .replace(/&gt;/g, '>')
     .replace(/&quot;/g, '"')
-    .replace(/&#039;/g, "'");
+    .replace(/&#039;/g, "'")
+    .replace(/&amp;/g, "&");
 
   return correctedText;
 }
