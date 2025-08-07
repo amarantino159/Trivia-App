13a136ed5337e4866fe9ae54aa9698d0f6336639
Samuel Angarole <samuelangarole@gmail.com>
2025-08-07 17:56:40 +0300

    added next button on question and answer

--- src/component/QuestionAndAnswer.jsx
+++ src/component/QuestionAndAnswer.jsx
@@ -8,37 +8,42 @@ import Answer from './Answer.jsx'
 import AnswerShort from './AnswerShort.jsx'
 
 const dummy = {
-    "type": "short",
-    // "type": "multiple",
-    "difficulty": "medium",
-    "category": "Vehicles",
-    "question": "Which of these cars is NOT considered one of the 5 Modern Supercars by Ferrari?",
-    "correct_answer": "Testarossa",
-    "incorrect_answers": [
-      "Enzo Ferrari",
-      "F40",
-      "288 GTO"
-    ]
-  };
+  "type": "short",
+  // "type": "multiple",
+  "difficulty": "medium",
+  "category": "Vehicles",
+  "question": "Which of these cars is NOT considered one of the 5 Modern Supercars by Ferrari?",
+  "correct_answer": "Testarossa",
+  "incorrect_answers": [
+    "Enzo Ferrari",
+    "F40",
+    "288 GTO"
+  ]
+};
 
-function QuestionAndAnswer({questionCard=dummy}){
+function QuestionAndAnswer({ questionCard = dummy }) {
+
   if(questionCard.type == 'boolean'||questionCard.type=='multiple')
   {
     return (<>
     <Question questionText={questionCard.question}/>
     <Answer answer={questionCard.correct_answer} falseanswers={questionCard.incorrect_answers} />
+      <button  >Next</button>
     </>)
   }
   else if(questionCard.type =='short'){
     return (<>
     <Question questionText={questionCard.question}/>
-    <AnswerShort answer={questionCard.correct_answer} />
+      <AnswerShort answer={questionCard.correct_answer} />
+      <button  >Next</button>
+
     </>)
   }
   else{
     return (<>
     <Question questionText={'Where is the Eiffel Tower?'}/>
-    <Answer answer={'Paris'} falseanswers={['London','Dubai','Los Angeles','Miami']} />
+      <Answer answer={'Paris'} falseanswers={['London', 'Dubai', 'Los Angeles', 'Miami']} />
+      <button  >Next</button>
     </>)
   }
 
